acd1b57b1721c5011b4e5fdf4cff837e137f7ef8
monalisaolivr <144165764+monalisaolivr@users.noreply.github.com>
2023-11-06 20:41:25 -0300

    add media query

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("text").innerHTML = "Eu também não sei sua senha &#128531";
+}
+
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+  
+}
